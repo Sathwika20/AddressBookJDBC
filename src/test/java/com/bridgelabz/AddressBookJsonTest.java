2eb9b4eadf21fe99d9a60f63c27d91b40f2fe7e6
Sathwika20 <gopirajsathwika1999@gmail.com>
2021-04-09 19:01:45 +0530

    Added code to add person details from json server

--- src/test/java/com/bridgelabz/AddressBookJsonTest.java
+++ src/test/java/com/bridgelabz/AddressBookJsonTest.java
@@ -1,6 +1,7 @@
 package com.bridgelabz;
 
 import com.mysql.fabric.Response;
+import jdk.jfr.ContentType;
 import org.junit.Assert;
 import org.junit.Test;
 
@@ -10,5 +11,16 @@ public class AddressBookJsonTest {
         Response response = RestAssured.get("http://localhost:3000/addressBook");
         System.out.println(response.asString());
         Assert.assertEquals(200, response.getStatusCode());
+    }
+    @Test
+    public void testToContactDataInJSONServer(){
+        Response response = RestAssured.given()
+                .contentType(ContentType.JSON)
+                .accept(ContentType.JSON)
+                .body("{\"firstName\": \"Sathwika\",\"lastName\": \"Gopiraj\",\"address\": \"HarithaEnclave\",\"city\": \"Kamareddy\",\"state\": \"Telangana\",\"zip\": \"503111\",\"phoneNumber\": \"7036101828\",\"email\": \"sathwika@gmail.com\",\"personType\": \"Friend\"}")
+                .when().post("http://localhost:3000/addressbook");
+        response.then()
+                .body("firstName", Matchers.is("Sathwika"));
+        Assert.assertEquals(201, response.getStatusCode());
     }
 }
